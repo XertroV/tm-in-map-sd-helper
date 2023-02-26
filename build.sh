3e708f8a51dd7a2056ac327478ad3901827d54da
Max Kaye <m@xk.io>
2023-02-26 20:54:09 +1100

    init version, works!

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cat SdHelper-pre > SdHelper.Script.txt
+cat SdHelper-main.Script.txt >> SdHelper.Script.txt
+cat SdHelper-post >> SdHelper.Script.txt
+
+echo "Updated SdHelper"
